762904169ccb3dfb509a2ddca487ae4b5fd4760b
Ivanciu Andrei <cheseecheseechesee@gmail.com>
2022-02-22 00:05:45 +0200

    Removed If
    removed if , to check if the bot gives out the role based on reaction to any message

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //bot token
-const token = process.env.TOKEN
+const token = process.env.TOKEN;
+
+//Discord Ids saved into consts
+const messageID='944644957975748658';
+const emoji_Y=':regional_indicator_y:';
+const emoji_N=':regional_indicator_n:';
+const kadan_role='944655004441128980';
+const not_kadan_role='943437093009498162';
+
 
 //message to be posted in the channel. 
 // const role_assign_message = 'If you are from Kadan server react to <process.env.Y_EMOJI_ID>, if you are not from Kadan react to <process.env.N_EMOJI_ID> ';
@@ -27,21 +35,38 @@ client.once('ready', ()=> {
 
 client.on('messageReactionAdd', async(reaction, user)=>{
     await reaction.fetch();
+    console.log(reaction);
+
+    if (reaction.emoji.name === emoji_Y) {
+         reaction.message.guild.members.cache.get(user.id).roles.add(kadan_role);
+         reaction.message.guild.members.cache.get(user.id).roles.remove(not_kadan_role);
+        console.log('kadan role assigned and not kadan role removed');
+    }
     
-    if(!reaction.message.id(process.env.KADAN_CHECK)){
-        return
+    if (reaction.emoji.name === emoji_N) {
+         reaction.message.guild.members.cache.get(user.id).roles.add(not_kadan_role);
+         reaction.message.guild.members.cache.get(user.id).roles.remove(kadan_role);
+        console.log('not kadan role assigned and kadan role removed');
     }
-    else if(reaction.message.id(process.env.KADAN_CHECK)){
+    
+
+
+    // if(!reaction.message.id(messageID)){
+    //     return
+    // }
+    // else if(reaction.message.id(messageID)){
         
-        if (reaction.emoji.id(process.env.Y_EMOJI_ID)) {
-            reaction.message.guild.members.cache.get(user.id).roles.add(process.env.KADAN_ROLE);
-            reaction.message.guild.members.cache.get(user.id).roles.remove(process.env.NOT_KADAN_ROLE);
-        }
-        if (reaction.emoji.id(process.env.N_EMOJI_ID)) {
-            reaction.message.guild.members.cache.get(user.id).roles.add(process.env.NOT_KADAN_ROLE);
-            reaction.message.guild.members.cache.get(user.id).roles.remove(process.env.KADAN_ROLE);
-        }
-    }
+    //     if (reaction.emoji.id(emoji_Y)) {
+    //         reaction.message.guild.members.cache.get(user.id).roles.add(kadan_role);
+    //         reaction.message.guild.members.cache.get(user.id).roles.remove(not_kadan_role);
+    //         console.log('kadan role assigned and not kadan role removed');
+    //     }
+    //     if (reaction.emoji.id(emoji_N)) {
+    //         reaction.message.guild.members.cache.get(user.id).roles.add(not_kadan_role);
+    //         reaction.message.guild.members.cache.get(user.id).roles.remove(kadan_role);
+    //         console.log('not kadan role assigned and kadan role removed');
+    //     }
+    // }
 
 });
 
